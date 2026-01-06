8d61bc4eb3ef1af8b20216ee1b4b41be4b069445
rsazidur <rsazidur936@gmail.com>
2026-01-06 15:47:49 +0600

    initial commit

---
+++ Front-End/src/sections/Header.tsx
@@ -0,0 +1,10 @@
+export const Header = () => {
+  return <div>
+    <nav>
+      <a href="#home">Home</a>
+      <a href="#about">About</a>
+      <a href="#projects">Projects</a>
+      <a href="#contact">Contact</a>
+    </nav>
+  </div>;
+};
