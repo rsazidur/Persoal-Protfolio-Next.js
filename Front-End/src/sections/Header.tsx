b9387277d44bca947612f165c71686ee41047c8b
rsazidur <rsazidur936@gmail.com>
2026-01-06 18:11:47 +0600

    Header Section

--- Front-End/src/sections/Header.tsx
+++ Front-End/src/sections/Header.tsx
@@ -1,10 +1,18 @@
 export const Header = () => {
-  return <div>
-    <nav>
-      <a href="#home">Home</a>
-      <a href="#about">About</a>
-      <a href="#projects">Projects</a>
-      <a href="#contact">Contact</a>
+  return <div className="flex justify-center items-center fixed top-3 w-full">
+    <nav className="flex gap-1 p-0.5 border border-white/15 rounded-full bg-white/10 backdrop-blur">
+      <a href="#home" className="nav-item">
+        Home
+      </a>
+      <a href="#about" className="nav-item">
+        About
+      </a>
+      <a href="#projects" className="nav-item">
+        Projects
+      </a>
+      <a href="#contact" className="nav-item bg-white text-gray-900 hover:bg-white/70 hover:text-gray-900">
+        Contact
+      </a>
     </nav>
   </div>;
 };
--- Front-End/src/sections/Hero.tsx
+++ Front-End/src/sections/Hero.tsx
@@ -1,3 +1,34 @@
+import memojImage from "@/assets/images/memoji-computer.png";
+import Image from "next/image";
+import ArrowDown from "@/assets/icons/arrow-down.svg";
+
 export const HeroSection = () => {
-  return <div>Hero Section</div>;
+  return (
+  <div className="py-32">
+    <div className="container">
+      <div className="flex flex-col items-center">
+        <Image src={memojImage} alt="Person peeking from behind laptop" />
+        <div className="bg-gray-950 border border-gray-800 px-4 py-1.5 inline-flex items-center gap-4 rounded-large">
+          <div className="bg-green-500 size-2.5 rounded-full"></div>
+          <div className="text-sm font-medium">Available for new projects</div>
+        </div>
+      </div>
+      <h1>Building Exceptional User Experiences</h1>
+      <p>
+        I am a passionate front-end developer dedicated to crafting beautiful and functional web applications. 
+        Let's work together to bring your ideas to life!
+      </p>
+      <div>
+        <button>
+          <span>Explore My Work</span>
+          <ArrowDown />
+        </button>
+        <button>
+          <span>👋</span>
+          <span>Let's Connect</span>
+        </button>
+      </div>
+    </div>
+  </div>
+  );
 };
